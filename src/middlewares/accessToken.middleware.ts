75eb72cdc49bed19bbfb50bfc8228281308675f1
Iryna Hrynko <ir.hrynko@gmail.com>
2022-08-11 10:37:18 +0200

    add auth

--- src/middlewares/accessToken.middleware.ts
+++ src/middlewares/accessToken.middleware.ts
@@ -1,13 +1,13 @@
-// import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-// function accessTokenMiddleware(req: Request, _res: Response, next: NextFunction) {
-//   const token = req.headers['Authorization'];
-//   if (token) {
-//     req.context = {
-//       token: String(token),
-//     };
-//   }
-//   next();
-// }
+function accessTokenMiddleware(req: Request, _res: Response, next: NextFunction) {
+  const token = req.headers['Authorization'];
+  if (token) {
+    req.context = {
+      token: String(token),
+    };
+  }
+  next();
+}
 
-// export default accessTokenMiddleware;
+export default accessTokenMiddleware;
